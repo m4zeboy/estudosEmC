6516c1e92e6f1892cfeaa3d4cabdf9e249f789fa
m4zeboy <moises775azevedo@gmail.com>
2022-12-06 10:56:43 -0300

    Simple Linked List - Insert end

--- lista.c
+++ lista.c
@@ -15,6 +15,25 @@ void insertStart(Node **list, int data) {
   }
 }
 
+void insertEnd(Node **list, int data) {
+  Node *new = (Node *) malloc(sizeof(Node));
+  if(new) {
+    Node *p, *q;
+    p = NULL; q = *list;
+    new->data = data;
+    new->next = NULL;
+    while(q != NULL) {
+      p = q;
+      q = q->next;
+    }
+    if(p == NULL) {
+      *list = new;
+    } else {
+      p->next = new;
+    }
+  }
+}
+
 void printList(Node *list) {
   if(list) {
     printf("%d -> ", list->data);
@@ -24,10 +43,10 @@ void printList(Node *list) {
 
 int main(void) {
   Node *list = NULL;
-  insertStart(&list, 1);
-  insertStart(&list, 2);
-  insertStart(&list, 3);
-  insertStart(&list, 4);
+  insertEnd(&list, 1);
+  insertEnd(&list, 2);
+  insertEnd(&list, 3);
+  insertEnd(&list, 4);
   printList(list);
   return 0;
 }
